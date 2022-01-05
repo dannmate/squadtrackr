d06f72686e364e299c54f1496f602ffa245c65f8
dananalyticlabs <dan.petrolito@analyticlabs.io>
2022-01-05 20:20:13 +1100

    commit first working version

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_voice_state_update(member, before, after):
+    if not before.channel and after.channel is not None:
+        channelTo = None
+        guild = discord.utils.find(lambda g: g.id == member.guild.id, client.guilds)
+        for channel in guild.channels:
+            if str(channel.type) == 'text':
+                channelTo = client.get_channel(channel.id)
+        await channelTo.send(f'{member.name} is now hanging out in {after.channel.name}!')
+
+client.run(TOKEN)
